0c8ee7e9518ce9464d9a8f66b3935c6a8966fc46
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-14 10:41:10 +0200

    RaktarElem hasznalata

--- Raktarkezelo/Program.cs
+++ Raktarkezelo/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Raktarkezelo;
+
+RaktarElem elem = new RaktarElem("Alma", 10);
+
+elem.Bevetelezes(5);
+elem.Bevetelezes(-4);
+
+elem.Kiadas(-7);
+elem.Kiadas(99);
+elem.Kiadas(4);
+
+int almaAr = 80;
+Console.WriteLine($"Tetszőleges brutto ár, {almaAr} Forinttal számítva: {RaktarElem.BruttoArSzamitas(almaAr)}");
