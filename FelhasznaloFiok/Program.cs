bf247e0a4d42161a6f89f543a24585d5d008fd92
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-14 10:31:46 +0200

    Felhasznalo hasznalata a Programban

--- FelhasznaloFiok/Program.cs
+++ FelhasznaloFiok/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using FelhasznaloFiok;
+
+Felhasznalo felhasznalo = new Felhasznalo("Lapos Elemér", "19840101");
+
+felhasznalo.JelszoModositas("19840101", "ujjelszo");
+felhasznalo.JelszoModositas("rossz jelszo", "masikjelszo");
