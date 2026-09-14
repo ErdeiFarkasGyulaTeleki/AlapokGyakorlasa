0558c8e78456d7c0b1c95dacc0a0f9d79b135486
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-14 10:19:43 +0200

    Termek hasznalata a Programban

--- WebaruhazTermek/Program.cs
+++ WebaruhazTermek/Program.cs
@@ -1,2 +1,11 @@
 ﻿using WebaruhazTermek;
 
+Termek termek = new Termek("Teszt Termék", 100, 10);
+termek.Kiiras();
+
+termek.Egysegar = -50;
+termek.Kiiras();
+
+termek.Keszlet = -5;
+termek.Kiiras();
+
