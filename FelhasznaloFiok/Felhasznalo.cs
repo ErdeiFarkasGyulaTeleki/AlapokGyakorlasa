c2c7c2dcbcb0694aa97ea9e4f3da22a48ccb8f6d
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-14 10:31:36 +0200

    Felhasznalo fejlesztes

--- FelhasznaloFiok/Felhasznalo.cs
+++ FelhasznaloFiok/Felhasznalo.cs
@@ -6,5 +6,26 @@ namespace FelhasznaloFiok
 {
     public class Felhasznalo
     {
+        private string jelszo = "123456";
+        public string FelhasznaloNev;
+
+        public void JelszoModositas(string regiJelszo, string ujJelszo)
+        {
+            if (regiJelszo == jelszo && ujJelszo.Length >= 6)
+            {
+                jelszo = ujJelszo;
+                Console.WriteLine("Sikeres jelszóváltás!");
+            }
+            else
+            {
+                Console.WriteLine("Sikertlen jelszóváltás!");
+            }
+        }
+
+        public Felhasznalo(string felhasznaloNev, string _jelszo)
+        {
+            FelhasznaloNev = felhasznaloNev;
+            jelszo = _jelszo;
+        }
     }
 }
