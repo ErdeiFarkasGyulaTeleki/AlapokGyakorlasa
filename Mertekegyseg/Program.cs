3635890a52c4eba619afcb451796b5cde47875a0
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-14 10:24:53 +0200

    Atvalto hasznalata

--- Mertekegyseg/Program.cs
+++ Mertekegyseg/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Mertekegyseg;
+
+Console.WriteLine(Atvalto.KmBolMeter(39));
+
+Atvalto atvalto = new Atvalto(216);
+Console.WriteLine(atvalto.EgyediAtvaltas(39));
