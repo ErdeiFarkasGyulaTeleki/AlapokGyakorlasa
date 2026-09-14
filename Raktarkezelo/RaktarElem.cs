1e7d32fcf970aa12346f793593d53490dfbc4e21
ErdeiFarkasGyulaTeleki <erdei.farkas.2022d@telekimezotur.hu>
2026-09-14 10:36:54 +0200

    RaktarElem megirasa

--- Raktarkezelo/RaktarElem.cs
+++ Raktarkezelo/RaktarElem.cs
@@ -6,5 +6,48 @@ namespace Raktarkezelo
 {
     public class RaktarElem
     {
+        private int dbSzam;
+        public string CikkNev { get; set; }
+
+        public static readonly double AFA_KULCS = 0.27;
+
+        public RaktarElem(string _cikkNev, int _dbSzam)
+        {
+            CikkNev = _cikkNev;
+            dbSzam = _dbSzam;
+        }
+
+        public void Bevetelezes(int db)
+        {
+            if (db < 1)
+            {
+                Console.WriteLine("Nem vehet be nempozitív mennyiséget!");
+                return;
+            }
+
+            dbSzam += db;
+        }
+
+        public void Kiadas(int db)
+        {
+            if (db < 1)
+            {
+                Console.WriteLine("Nem vehet ki nempozitív mennyiséget!");
+                return;
+            }
+
+            if (db > dbSzam)
+            {
+                Console.WriteLine("Nem lehet kivenni több terméket, mint amennyi van a raktárban!");
+                return;
+            }
+
+            dbSzam -= db;
+        }
+
+        public static double BruttoArSzamitas(double nettoAr)
+        {
+            return nettoAr * (1 + AFA_KULCS);
+        }
     }
 }
